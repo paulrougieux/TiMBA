44d4c8372de533bb50d0129f3c8ea015ca9affda
Paul Rougieux <paul.rougieux@gmail.com>
2025-09-03 17:29:27 +0200

    TIMBA_DATA_DIR defines a path to the data. See issue TI-Forest-Sector-Modelling/TiMBA#72

---
+++ TiMBA/__init__.py
@@ -0,0 +1,16 @@
+"""
+This is where the main page of the auto generated documentation website can be.
+
+`TIMBA_DATA_DIR` can be either pre-defined and hard coded in the default case,
+or it can be defined through an environmental variable.
+
+"""
+
+from pathlib import Path
+
+# Path do the data, default case
+TIMBA_DATA_DIR = Path.home() / "timba_data"
+# Or defined through an environmental
+if os.environ.get("TIMBA_DATA_DIR"):
+    TIMBA_DATA_DIR = Path(os.environ["TIMBA_DATA_DIR"])
+
