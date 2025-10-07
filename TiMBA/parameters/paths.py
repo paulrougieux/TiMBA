d75225daac10ec121dfac62f20cd4ee3c8585a1d
Paul Rougieux <paul.rougieux@gmail.com>
2025-10-07 23:08:53 +0200

    Start building paths upon the TIMBA_DATA_DIR variable

--- TiMBA/parameters/paths.py
+++ TiMBA/parameters/paths.py
@@ -1,23 +1,24 @@
 import datetime as dt
+from TiMBA import TIMBA_DATA_DIR
 
 current_dt = dt.datetime.now().strftime("%Y%m%dT%H-%M-%S")
 
 # input
-INPUT_WORLD_PATH = r"data/input/01_Input_Files/"
-ADDITIONAL_INFORMATION_PATH = r"data/input/02_Additional_Information/additional_information.xlsx"
-WORLDPRICE_PATH = r"data/input/02_Additional_Information/worldprice_world500.xlsx"
-PKL_WORLD_PATH = r"data/input/03_Serialization/WorldDataContent.pkl"
-PKL_ADD_INFO_PATH = r"data/input/03_Serialization/AddInfoContent.pkl"
-PKL_WORLDPRICE_PATH = r"data/input/03_Serialization/WorldPriceContent.pkl"
+INPUT_WORLD_PATH = TIMBA_DATA_DIR / "input/01_Input_Files/"
+ADDITIONAL_INFORMATION_PATH = TIMBA_DATA_DIR / "input/02_Additional_Information/additional_information.xlsx"
+WORLDPRICE_PATH = TIMBA_DATA_DIR / "input/02_Additional_Information/worldprice_world500.xlsx"
+PKL_WORLD_PATH = TIMBA_DATA_DIR / "input/03_Serialization/WorldDataContent.pkl"
+PKL_ADD_INFO_PATH = TIMBA_DATA_DIR / "input/03_Serialization/AddInfoContent.pkl"
+PKL_WORLDPRICE_PATH = TIMBA_DATA_DIR / "input/03_Serialization/WorldPriceContent.pkl"
 
 # output
-RESULTS_OUTPUT = "data/output/results_D"
-RESULTS_OUTPUT_AGG = "data/output/results_aggregated_D"
-FOREST_OUTPUT = "data/output/forest_D"
-WORLD_PRICE_OUTPUT = "data/output/world_prices_D"
-MANUFACTURE_OUTPUT = "data/output/manufacture_D"
-LOGGING_OUTPUT_FOLDER = r"data/output"
-PKL_OUTPUT_PATH = r"data/output/pkl"
+RESULTS_OUTPUT = TIMBA_DATA_DIR / "output/results_D"
+RESULTS_OUTPUT_AGG = TIMBA_DATA_DIR / "output/results_aggregated_D"
+FOREST_OUTPUT = TIMBA_DATA_DIR / "output/forest_D"
+WORLD_PRICE_OUTPUT = TIMBA_DATA_DIR / "output/world_prices_D"
+MANUFACTURE_OUTPUT = TIMBA_DATA_DIR / "output/manufacture_D"
+LOGGING_OUTPUT_FOLDER = TIMBA_DATA_DIR / "output"
+PKL_OUTPUT_PATH = TIMBA_DATA_DIR / "output/pkl"
 
 # plot
 OUTPUT_FILE = "\\data\\output\\results_D*.csv"
